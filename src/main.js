8548f9b0365401313de0805fe863238c31cf6079
gustavo quinalha <gustavo_quinalha@hotmail.com>
2017-12-08 13:19:26 -0200

    remove hgjs

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import vueHljs from 'vue-hljs/dist/vue-hljs.min'
-
-Vue.use(vueHljs)
 
 Vue.config.productionTip = false
 
